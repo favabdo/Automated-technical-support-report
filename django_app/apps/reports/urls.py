7440a605a46a0d2e970eafc85c6f3378d5db9f2c
Abdullah Elsawy <abdallah666mo@gmail.com>
2026-06-06 04:26:16 +0300

    Fix reports urls -2

--- django_app/apps/reports/urls.py
+++ django_app/apps/reports/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reports_list, name='reports_list'),
-    path('agents/', views.agents_list, name='agents_list'),
-    path('agents/<int:agent_id>/', views.agent_detail, name='agent_detail'),
-    path('customers/', views.customers_list, name='customers_list'),
-    path('customers/<int:customer_id>/', views.customer_detail, name='customer_detail'),
-    path('monthly/', views.monthly, name='monthly'),
+    path('',         views.reports_list, name='reports_list'),
+    path('monthly/', views.monthly,      name='monthly'),
 ]
